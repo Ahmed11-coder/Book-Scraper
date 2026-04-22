fb2fbee1b8530c66dd4d88d31a4786938f62207c
Ahmed <aa87006722@gmail.com>
2026-04-22 14:08:25 +0200

    ( Modify ) Get All Books Title, Prices and Rating Bases On Page

--- scraper.py
+++ scraper.py
@@ -6,7 +6,24 @@ url = "https://books.toscrape.com/"
 def scrape_books(page = 1):
     url = f"https://books.toscrape.com/catalogue/page-{page}.html"
     response = requests.get(url)
-
+    books = []
 
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.prettify())
+    
+    boxs = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+    for box in boxs:
+        title = box.find("h3").find("a")["title"]
+        price = box.find('p', class_="price_color").text
+        rating = box.find('p', class_="star-rating")["class"][1]
+        books.append({
+            "title": title,
+            "price": price,
+            "rating": rating
+        })
+
+    return books
+
+
+
+scraped_books = scrape_books()
+print(scraped_books)

b287581476fe0a535002826246b409f8323fedb1
Ahmed <aa87006722@gmail.com>
2026-04-22 15:08:40 +0200

    ( Build ) Start Menu To Navigate Between Features

---
+++ main.py
@@ -0,0 +1,29 @@
+from analyzer import save_data
+from scraper import scrape_in_range
+
+
+print("======== Scraping Books =======")
+print("Menu:")
+print("1. Scrape New Data")
+print("2. Show Summary")
+print("3. Show Chart")
+print("4. Filter Books")
+print("5. Exit")
+
+
+while True:
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        start_page = int(input("Enter the start page: "))
+        end_page = int(input("Enter the end page: "))
+
+        scraped_books = scrape_in_range(start_page, end_page)
+        save_data(scraped_books)
+
+    elif choice >= 5:
+        print("Exiting...")
+        break
+    
+    else :
+        print("Option not implemented yet.")
